ee3a86774fe707cd62220fd0003d12cc7084c6f0
miniengineer <samadova.minira@gmail.com>
2020-12-03 10:11:40 +0900

    add Bottles class implementation

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -14,7 +14,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_another_verse
-    skip
     expected =
       "3 bottles of milk on the wall, " +
       "3 bottles of milk.\n" +
@@ -24,7 +23,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_2
-    skip
     expected =
       "2 bottles of milk on the wall, " +
       "2 bottles of milk.\n" +
@@ -34,7 +32,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_1
-    skip
     expected =
       "1 bottle of milk on the wall, " +
       "1 bottle of milk.\n" +
@@ -44,7 +41,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_0
-    skip
     expected =
       "No more bottles of milk on the wall, " +
       "no more bottles of milk.\n" +
@@ -54,7 +50,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_a_couple_verses
-    skip
     expected =
       "99 bottles of milk on the wall, " +
       "99 bottles of milk.\n" +
@@ -69,7 +64,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_a_few_verses
-    skip
     expected =
       "2 bottles of milk on the wall, " +
       "2 bottles of milk.\n" +
@@ -89,7 +83,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_the_whole_song
-    skip
     expected = <<~SONG
       99 bottles of milk on the wall, 99 bottles of milk.
       Take one down and pass it around, 98 bottles of milk on the wall.
